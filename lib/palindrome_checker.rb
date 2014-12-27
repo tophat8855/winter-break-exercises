3871a99043afab29548e4a6fda56960414a9d974
tophat8855 <hrfarley@gmail.com>
2014-12-26 20:40:17 -0800

    Plaindrome Checker exercise

--- lib/palindrome_checker.rb
+++ lib/palindrome_checker.rb
@@ -15,9 +15,25 @@ class PalindromeChecker
     end
     @string
   end
+
+  def check_palindrome
+    save_the_string = @string.clone
+    compare_to_this_string = self.switchy
+    if save_the_string == compare_to_this_string
+      "#{@string} is a palindrome"
+    else
+      "#{save_the_string} is NOT a palindrome"
+    end
+
+  end
 end
 
-puts "Enter a string"
+puts "Enter a string for flipping"
 string = gets.chomp
 switch_this = PalindromeChecker.new(string)
 puts switch_this.switchy
+
+puts "Enter a string to check for palindrome-ness"
+string = gets.chomp
+check_this = PalindromeChecker.new(string)
+puts check_this.check_palindrome
--- spec/palindrome_checker_spec.rb
+++ spec/palindrome_checker_spec.rb
@@ -2,10 +2,24 @@ require 'spec_helper'
 require 'palindrome_checker'
 
 describe PalindromeChecker do
-  it "'reverse' returns the reverse of the given string" do
+  it "'switchy' returns the reverse of the given string" do
     flip_this = PalindromeChecker.new("hello")
     result = flip_this.switchy
 
     expect(result).to eq("olleh")
+  end
+
+  it "checks for palindrome-ness and tells you if not" do
+    pali = PalindromeChecker.new("goodbye")
+    result = pali.check_palindrome
+
+    expect(result).to include ("is NOT a palindrome")
+  end
+
+  it "checks for palindrome-ness and tells you if so" do
+    pali = PalindromeChecker.new("hannah")
+    result = pali.check_palindrome
+
+    expect(result).to include ("is a palindrome")
   end
 end
