04b5d4e97f9e49070f1ed3969cf0464ba829fc13
tophat8855 <hrfarley@gmail.com>
2014-12-26 20:03:22 -0800

    reverse a string exercise

---
+++ lib/palindrome_checker.rb
@@ -0,0 +1,22 @@
+class PalindromeChecker
+  def initialize(string)
+    @string = string
+  end
+
+  def reverse
+    outer_index = 0
+    while outer_index < (@string.size - 1)
+      inner_index = 0
+      while inner_index < (@string.size - outer_index - 1)
+        @string[inner_index], @string[inner_index + 1] = @string[inner_index + 1], @string[inner_index]
+        inner_index += 1
+      end
+      outer_index += 1
+    end
+    @string
+  end
+end
+
+puts "Enter a string"
+string = gets.chomp
+puts string.reverse
---
+++ spec/palindrome_checker_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'palindrome_checker'
+
+describe PalindromeChecker do
+  it "'reverse' returns the reverse of the given string" do
+    flip_this = PalindromeChecker.new("hello")
+    result = flip_this.reverse
+
+    expect(result).to eq("olleh")
+  end
+end
