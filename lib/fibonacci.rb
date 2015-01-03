056e2fc1b81ed6fde47769bd6d89245de3612f30
tophat8855 <hrfarley@gmail.com>
2015-01-02 21:32:10 -0800

    fibonacci exercise

---
+++ lib/fibonacci.rb
@@ -0,0 +1,12 @@
+fibonacci_array = []
+
+first = 0
+second = 1
+
+while fibonacci_array.size < 100
+  new_sum = first + second
+  first = second
+  second = new_sum
+  fibonacci_array << new_sum
+end
+p fibonacci_array
